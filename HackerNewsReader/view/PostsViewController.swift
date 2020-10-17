11d8580db78840bdb1e387f1ab1560dbc2d2d928
Claudio Roitman <claudio.roitman@gmail.com>
2020-10-17 18:36:16 -0300

    Hides navigation bar in first screen, as design suggests

--- HackerNewsReader/view/PostsViewController.swift
+++ HackerNewsReader/view/PostsViewController.swift
@@ -33,6 +33,19 @@ class PostsViewController: UIViewController {
         refreshControl.attributedTitle = NSAttributedString(string: viewModel.refreshTitle)
         
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
     
     // MARK: - Actions
     @objc func refresh(_ sender: AnyObject) {
